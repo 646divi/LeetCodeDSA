6ddca9b1003684206cb34624066b428ab564f5c2
Devesh Udhlani <80637738+646divi@users.noreply.github.com>
2023-04-22 17:00:03 +0530

    Added solution - LeetHub

--- Geek Jump - GFG/geek-jump.cpp
+++ Geek Jump - GFG/geek-jump.cpp
@@ -21,19 +21,33 @@ class Solution {
     //Tabulation approach which is also known as bottom up DP
     int minimumEnergy(vector<int>& height, int n) {
         // Code here
+        // if(n<0)return 0;
+        // vector<int>dp(n+1,0);
+        // // return FindEnergy(height,n-1,dp);
+        // dp[0]=0;
+        // for(int i=1;i<n;i++){
+        //     int oneForward=dp[i-1]+abs(height[i]-height[i-1]);
+        //     int twoForward=INT_MAX;
+        //     if(i>1)twoForward=dp[i-2]+abs(height[i]-height[i-2]);
+        //     dp[i]=min(oneForward,twoForward);
+        // }
+        
+        
+        // return dp[n-1];
+        
+        //Space optimized approach
         if(n<0)return 0;
-        vector<int>dp(n+1,0);
-        // return FindEnergy(height,n-1,dp);
-        dp[0]=0;
+        int prev1=0,prev2=0;
         for(int i=1;i<n;i++){
-            int oneForward=dp[i-1]+abs(height[i]-height[i-1]);
-            int twoForward=INT_MAX;
-            if(i>1)twoForward=dp[i-2]+abs(height[i]-height[i-2]);
-            dp[i]=min(oneForward,twoForward);
+            int left=prev1+abs(height[i]-height[i-1]);
+            int right=INT_MAX;
+            if(i>1)right=prev2+abs(height[i]-height[i-2]);
+            
+            int curr=min(left,right);
+            prev2=prev1;
+            prev1=curr;
         }
-        
-        
-        return dp[n-1];
+        return prev1;
     }
 };
 
