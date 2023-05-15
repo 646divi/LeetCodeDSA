c0bccb8b05d176835ecbe702496f845cd50f1db4
Devesh Udhlani <80637738+646divi@users.noreply.github.com>
2023-05-15 06:43:30 +0530

    Added solution - LeetHub

--- Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
+++ Knapsack with Duplicate Items - GFG/knapsack-with-duplicate-items.cpp
@@ -22,8 +22,19 @@ public:
     int knapSack(int N, int W, int val[], int wt[])
     {
         // code here
-        vector<vector<int>>dp(N,vector<int>(W+1,-1));
-        return solve(N-1,W,val,wt,dp);
+        vector<vector<int>>dp(N,vector<int>(W+1,0));
+        // return solve(N-1,W,val,wt,dp);
+        // for(int i=0;i<N;i++)dp[i][0]=0;
+        for(int i=wt[0];i<=W;i++)dp[0][i]=((int) i/wt[0]) * val[0];
+        for(int i=1;i<N;i++){
+            for(int j=0;j<=W;j++){
+                int not_take=0+dp[i-1][j];
+                int take=INT_MIN;
+                if(wt[i]<=j)take=val[i]+dp[i][j-wt[i]];
+                dp[i][j]=max(take,not_take);
+            }
+        }
+        return dp[N-1][W];
     }
 };
 
