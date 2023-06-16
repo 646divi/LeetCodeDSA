743eec538c5f33a31e91570ec5a96a42251ccf7d
Devesh Udhlani <80637738+646divi@users.noreply.github.com>
2023-06-16 18:43:22 +0530

    Time: 95 ms (84.86%), Space: 56.8 MB (97.80%) - LeetHub

--- 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.cpp
+++ 0701-insert-into-a-binary-search-tree/0701-insert-into-a-binary-search-tree.cpp
@@ -15,11 +15,31 @@ public:
         if(!root){
             return new TreeNode(val);
         }
-        if(val < root->val ){
-            root->left=insertIntoBST(root->left,val);
-        }
-        if(val > root->val){
-            root->right=insertIntoBST(root->right,val);
+        // if(val < root->val ){
+        //     root->left=insertIntoBST(root->left,val);
+        // }
+        // if(val > root->val){
+        //     root->right=insertIntoBST(root->right,val);
+        // }
+        // return root;
+        TreeNode *curr=root;
+        while(curr){
+            if(val > curr->val){
+                if(curr->right){
+                    curr=curr->right;
+                }else{
+                    curr->right=new TreeNode(val);
+                    break;
+                }
+            }
+            if(val < curr->val){
+                if(curr->left){
+                    curr=curr->left;
+                }else{
+                    curr->left=new TreeNode(val);
+                    break;
+                }
+            }
         }
         return root;
     }
