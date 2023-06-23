e57924cb19d4f1ab0df9fe04111853b64113b46d
Devesh Udhlani <80637738+646divi@users.noreply.github.com>
2023-06-23 23:25:10 +0530

    Updated Solution - GfG to GitHub

--- Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -3,35 +3,79 @@
 using namespace std;
 
 // } Driver Code Ends
+
+
+class DisjointSet{
+    vector<int>rank,parent,size;
+    public:
+    DisjointSet(int V){
+        rank.resize(V+1,0);
+        parent.resize(V+1);
+        size.resize(V+1);
+        for(int i=0;i<=V;i++){
+            parent[i]=i;
+            size[i]=1;
+        }
+    }
+    int findParent(int x){
+        if(parent[x]==x)return x;
+        return parent[x]=findParent(parent[x]);
+    }
+    // void addEdges(int x,int y){
+    //     int boss_parentX=findParent(x);
+    //     int boss_parentY=findParent(y);
+    //     if(boss_parentX==boss_parentY)return;
+    //     if(rank[boss_parentX]==rank[boss_parentY]){
+    //         parent[boss_parentY]=boss_parentX;
+    //         rank[boss_parentX]++;
+    //     }else if(rank[boss_parentX] < rank[boss_parentY]){
+    //         parent[boss_parentY]=boss_parentX;
+    //     }else{
+    //         parent[boss_parentX]=boss_parentY;
+    //     }
+    // }
+    void unionBySize(int x,int y){
+        int ulp_x=findParent(x);
+        int ulp_y=findParent(y);
+        if(ulp_x==ulp_y)return;
+        if(size[ulp_x]==size[ulp_y]){
+            parent[ulp_y]=ulp_x;
+            size[ulp_x]+=size[ulp_y];
+        }else if(size[ulp_y] > size[ulp_x]){
+            parent[ulp_x]=ulp_y;
+            size[ulp_y]+=size[ulp_x];
+        }else{
+            parent[ulp_y]=ulp_x;
+            size[ulp_x]+=size[ulp_y];
+        }
+    }
+};
 class Solution
 {
 	public:
-	typedef pair<int,int> p;
+	//Krushkal's Algorithm
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
-    int spanningTree(int V, vector<vector<int>> adj[])
+    int spanningTree(int V, vector<vector<int>> vec[])
     {
         // code here
-        int vis[V]={0};
-        int sum=0;
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>>pq;
-        pq.push({0,0});
-        while(!pq.empty()){
-            auto it=pq.top();
-            pq.pop();
-            int wt=it.first;
-            int node=it.second;
-            if(vis[node]==1)continue;
-            vis[node]=1;
-            sum+=wt;
-            for(auto x:adj[node]){
-                if(!vis[x[0]]){
-                    pq.push({x[1],x[0]});
-                }
-            }
+        vector<pair<int,pair<int,int>>>adj;
+        for(int i=0;i<V;i++){
+            for(auto x:vec[i]){
+            adj.push_back({x[1],{i,x[0]}});
+           }
         }
         
-        return sum;
+        sort(adj.begin(),adj.end());
+        DisjointSet ds(V);
         
+        int mst=0;
+        for(auto x:adj){
+            if(ds.findParent(x.second.first)!=ds.findParent(x.second.second)){
+                mst+=x.first;
+                ds.unionBySize(x.second.first,x.second.second);
+            }
+        }
+        return mst;
         
     }
 };
