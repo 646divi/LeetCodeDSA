7940a35977961ce53d5305af6326f2a9e87477d9
Devesh Udhlani <80637738+646divi@users.noreply.github.com>
2023-07-07 04:43:10 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Maximum Rectangular Area in a Histogram/maximum-rectangular-area-in-a-histogram.cpp
@@ -0,0 +1,102 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to find largest rectangular area possible in a given histogram.
+    long long getMaxArea(long long arr[], int n)
+    {
+        // Your code here
+        // Naive approach 
+        // int area=INT_MIN;
+        // for(int i=0;i<n;i++){
+        //     int cnt=1;
+        //     if(i!=0){
+        //         int j;
+        //         for(j=i-1;j>=0;j--){
+        //             if(arr[j]<arr[i])break;
+        //         }
+        //         cnt+=(i - j - 1);
+                
+        //     }
+        //     if(i!=n-1){
+        //         int j;
+        //         for(j=i+1;j<n;j++){
+        //             if(arr[j]<arr[i])break;
+        //         }
+        //         cnt+=(j - i - 1);
+        //     }
+        //     // area=max(area,(cnt*arr[i]));
+        //     int new_area=cnt*arr[i];
+        //     if(area < new_area){
+        //         area=new_area;
+        //     }
+        
+        //another pproach using 2 aux arrays i.e pse and nse
+        vector<long long>pse(n);
+        vector<long long>nse(n);
+        // pse[0]=0;
+        // nse[n-1]=n;
+        stack<long long>st;
+        // int curr;
+        // st.push(0);
+        for(int i=0;i<n;i++){
+            while(!st.empty() and arr[i]<=arr[st.top()]){
+                st.pop();
+            }
+            if(st.empty())pse[i]=0;
+            else pse[i]=st.top()+1;
+            st.push(i);
+            
+        }
+        stack<long long>st2;
+        // st2.push(n-1);
+        for(int i=n-1;i>=0;i--){
+            while(!st2.empty() and arr[i]<=arr[st2.top()]){
+                st2.pop();
+            }
+            if(st2.empty())nse[i]=n-1;
+            else nse[i]=st2.top()-1;
+            st2.push(i);
+        }
+        long long area=0;
+        long long new_area=0;
+        for(int i=0;i<n;i++){
+            new_area=(nse[i] - pse[i] + 1)*1ll*arr[i];
+            // new_area*=arr[i];
+            if(area<new_area)area=new_area;
+        }
+            
+        
+        return area;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+ {
+    long long t;
+
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        
+        long long arr[n];
+        for(int i=0;i<n;i++)
+            cin>>arr[i];
+        Solution ob;
+        cout<<ob.getMaxArea(arr, n)<<endl;
+    
+    }
+	return 0;
+}
+
+// } Driver Code Ends
